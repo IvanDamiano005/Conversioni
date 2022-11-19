e830d227e94db1a7bb6b8a60e2b536714e179a92
ivand <ivand@Desktop_Ivan>
2022-11-19 09:32:41 +0100

    Commit

--- src/conversioni/UsaConversioni.java
+++ src/conversioni/UsaConversioni.java
@@ -10,15 +10,60 @@ import java.util.Scanner;
  */
 public class UsaConversioni {
     public static void main(String[] args) throws IOException{
-       System.out.println("Inserisci il numero: ");
-       Scanner tastiera = new Scanner(System.in);
-       String numero = tastiera.nextLine();
+        System.out.println("Inserisci la base di partenza del numero: ");
+        Scanner tastiera1 = new Scanner(System.in);
+        int basePartenza = tastiera1.nextInt();
        
-       System.out.println("Inserisci la base di partenza del numero");
-       Scanner tastiera2 = new Scanner(System.in);
-       int basePartenza = tastiera2.nextInt();
-       
-       int x = toDecimale(numero,basePartenza);
-       System.out.println("Il numero in base 10 e': " + x);
+        System.out.println("Inserisci il numero: ");
+        Scanner tastiera = new Scanner(System.in);
+        String numero = tastiera.nextLine();
+        if(basePartenza == 2){
+            System.out.println("Inserisci la base di arrivo: ");
+            Scanner tastiera2 = new Scanner(System.in);
+            int baseArrivo = tastiera2.nextInt();
+            if(baseArrivo == 8){
+           
+            }else if(baseArrivo == 10){
+               int x = toDecimale(numero,2);
+            }else if(baseArrivo == 16){
+           
+            }
+        }else if(basePartenza == 8){
+            System.out.println("Inserisci la base di arrivo: ");
+            Scanner tastiera2 = new Scanner(System.in);
+            int baseArrivo = tastiera2.nextInt();
+            if(baseArrivo == 2){
+           
+            }else if(baseArrivo == 10){
+               int x = toDecimale(numero,8);
+            }else if(baseArrivo == 16){
+           
+            }
+        }else if(basePartenza == 10){
+            System.out.println("Inserisci la base di arrivo: ");
+            Scanner tastiera2 = new Scanner(System.in);
+            int baseArrivo = tastiera2.nextInt();
+            if(baseArrivo == 2){
+           
+            }else if(baseArrivo == 8){
+               
+            }else if(baseArrivo == 16){
+           
+            }
+        }else if(basePartenza == 16){
+            System.out.println("Inserisci la base di arrivo: ");
+            Scanner tastiera2 = new Scanner(System.in);
+            int baseArrivo = tastiera2.nextInt();
+            if(baseArrivo == 2){
+           
+            }else if(baseArrivo == 8){
+                
+            }else if(baseArrivo == 10){
+                int x = toDecimale(numero,16);
+            }
+        }
+        int x = toDecimale(numero,basePartenza);
+        System.out.println("Il numero in base 10 e': " + x);
    }
 }
+    
