a10a95f0bc903749f2db2f0f5307b119d6c9f3ce
ivand <ivand@Desktop_Ivan>
2022-11-19 08:27:12 +0100

    First commit

--- src/conversioni/Conversioni.java
+++ src/conversioni/Conversioni.java
@@ -10,7 +10,7 @@ public class Conversioni {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
